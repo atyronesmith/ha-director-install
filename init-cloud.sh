030b1fcaaf78394ab70e9e7ce0a28d9101b24204
Andrew Bays <andrew.bays@gmail.com>
2017-02-14 10:12:58 -0500

    Created bonded SR-IOV stuff

--- init-cloud.sh
+++ init-cloud.sh
@@ -42,6 +42,20 @@ net_id=$(neutron net-list | grep sriov2 | awk '{print $2}')
 sub_id=$(neutron subnet-list | grep sriov2-sub | awk '{print $2}')
 neutron port-create --name sriov4-port --fixed-ip subnet_id=$sub_id,ip_address=172.22.0.11 --vnic-type direct $net_id
 
+neutron net-create --provider:network_type vlan --provider:physical_network phy-sriov1 --provider:segmentation_id 4075 sriov-for-bonds
+net_id=$(neutron net-list | grep sriov-for-bonds | awk '{print $2}')
+neutron subnet-create $net_id --allocation_pool start=172.30.0.2,end=172.30.1.0 --name sriov-for-bonds-sub 172.30.0.0/16
+sub_id=$(neutron subnet-list | grep sriov-for-bonds-sub | awk '{print $2}')
+neutron port-create --name sriov-bonded-port --fixed-ip subnet_id=$sub_id,ip_address=172.30.0.10 --vnic-type direct $net_id
+neutron port-create --name sriov-bonded-port3 --fixed-ip subnet_id=$sub_id,ip_address=172.30.0.12 --vnic-type direct $net_id
+
+neutron net-create --provider:network_type vlan --provider:physical_network phy-sriov2 --provider:segmentation_id 4075 sriov-for-bonds2
+net_id=$(neutron net-list | grep sriov-for-bonds2 | awk '{print $2}')
+neutron subnet-create $net_id --allocation_pool start=172.30.0.2,end=172.30.1.0 --name sriov-for-bonds-sub2 172.30.0.0/16
+sub_id=$(neutron subnet-list | grep sriov-for-bonds-sub2 | awk '{print $2}')
+neutron port-create --name sriov-bonded-port2 --fixed-ip subnet_id=$sub_id,ip_address=172.30.0.11 --vnic-type direct $net_id
+neutron port-create --name sriov-bonded-port4 --fixed-ip subnet_id=$sub_id,ip_address=172.30.0.13 --vnic-type direct $net_id
+
 openstack floating ip create external
 openstack floating ip create external
 openstack floating ip create external
