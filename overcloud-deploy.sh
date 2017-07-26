4677c53bc0677d57be15e6641ad3ca808de80997
Andrew Bays <abays@redhat.com>
2017-07-26 11:56:27 -0400

    First draft of SR-IOV + ODL

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -3,4 +3,6 @@ openstack overcloud deploy \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
 -e ./templates/environments/storage-environment.yaml \
+-e ~/templates/environments/neutron-sriov.yaml \
+-e ~/templates/environments/neutron-opendaylight-l3.yaml \
 -e ./templates/ips-from-pool-all.yaml
