c35d7e77440caf64a02597596f4b30ada763e267
Andrew Bays <abays@redhat.com>
2017-07-31 13:49:25 -0400

    Deploy succeeds

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -2,7 +2,6 @@ openstack overcloud deploy \
 --templates  \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
--e ./templates/environments/storage-environment.yaml \
--e ~/templates/environments/neutron-sriov.yaml \
--e ~/templates/environments/neutron-opendaylight-l3.yaml \
+-e ./templates/environments/neutron-sriov.yaml \
+-e ./templates/environments/neutron-opendaylight-l3.yaml \
 -e ./templates/ips-from-pool-all.yaml
