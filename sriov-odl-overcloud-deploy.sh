9f31d6945e2c779aab0d147099c0283df471483a
Andrew Bays <abays@redhat.com>
2017-08-12 08:03:15 -0400

    Remove sriov deploy script

--- sriov-odl-overcloud-deploy.sh
+++
@@ -1,8 +0,0 @@
-openstack overcloud deploy \
---templates  \
--e ./templates/environments/network-isolation.yaml \
--e ./templates/environments/network-environment.yaml \
--e ./templates/environments/storage-environment.yaml \
--e ~/templates/environments/neutron-sriov.yaml \
--e ~/templates/environments/neutron-opendaylight-l3.yaml \
--e ./templates/ips-from-pool-all.yaml
