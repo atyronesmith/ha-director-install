2978a4743b20229ea205d091ba211268ffe0c7da
Andrew Bays <abays@redhat.com>
2017-10-04 13:04:02 -0400

    Infrared OSP11 (not working)

--- overcloud-deploy.sh
+++
@@ -1,6 +0,0 @@
-openstack overcloud deploy \
---templates  \
--e ./templates/environments/network-isolation.yaml \
--e ./templates/environments/network-environment.yaml \
--e ./templates/environments/storage-environment.yaml \
--e ./templates/ips-from-pool-all.yaml
--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -2,7 +2,4 @@ openstack overcloud deploy \
 --templates  \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
--e ./templates/environments/storage-environment.yaml \
--e ~/templates/environments/neutron-sriov.yaml \
--e ~/templates/environments/neutron-opendaylight-l3.yaml \
 -e ./templates/ips-from-pool-all.yaml
