4169e12de083d8cce3ba809e659d4e525da9b2e1
Andrew Bays <abays@redhat.com>
2017-10-06 10:38:26 -0400

    Successful deploy via Infrared

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -2,4 +2,5 @@ openstack overcloud deploy \
 --templates  \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
+-e ./templates/environments/docker_registry.yaml \
 -e ./templates/ips-from-pool-all.yaml
