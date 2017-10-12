1e05390eeadbb4f4a921154ae51e8fee0315bb31
Andrew Bays <abays@redhat.com>
2017-10-12 14:21:28 -0400

    NICs on separate NUMAs

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -2,5 +2,4 @@ openstack overcloud deploy \
 --templates  \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
--e ./templates/environments/docker_registry.yaml \
 -e ./templates/ips-from-pool-all.yaml
