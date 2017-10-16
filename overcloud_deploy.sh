faa2485d61b22f4151963385d7bbea2860d6f9f1
Andrew Bays <abays@redhat.com>
2017-10-16 14:32:14 -0400

    Containerized deployment

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -2,5 +2,7 @@ openstack overcloud deploy \
 --templates  \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
 -e ./templates/environments/docker_registry.yaml \
 -e ./templates/ips-from-pool-all.yaml
