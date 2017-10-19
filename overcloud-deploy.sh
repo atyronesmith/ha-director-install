ca566594a743f93f0b7547612400d92d3f1609f5
Andrew Bays <abays@redhat.com>
2017-10-19 09:01:10 -0400

    Successful deployment (untested)

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -1,12 +1,8 @@
 openstack overcloud deploy \
 --templates  \
 -r ./templates/roles_data.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
--e ./templates/environments/docker_registry.yaml \
 -e ./templates/environments/neutron-sriov.yaml \
 -e ./templates/ips-from-pool-all.yaml
