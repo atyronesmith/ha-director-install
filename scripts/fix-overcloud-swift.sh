415e664154a754d17883b5fe60cc3882aabd4c0c
Andrew Bays <abays@redhat.com>
2017-10-24 11:19:21 -0400

    Successful deploy, successful intra-instance ping

--- scripts/fix-overcloud-swift.sh
+++ scripts/fix-overcloud-swift.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
 # 1. run on controller(s) in overcloud!
-# 2. find IPs by doing: swift-ring-builder object.ring.gz 
-# 3. find proper IPs via "ip a"
-# 4. run this script, passing bad IPs
+# 2. find proper IPs via "ip a"
+# 3. run this script, passing proper IPs
 
 first=$1
 second=$2
 third=$3
 
+if [[ -z $second ]] ; then
+  second="" ;
+fi
+
 if [[ -z $third ]] ; then
   third="" ;
 fi
 
-for i in $first $second $third; do sudo swift-ring-builder /etc/swift/account.builder remove $i; sudo swift-ring-builder /etc/swift/object.builder remove $i; sudo swift-ring-builder /etc/swift/container.builder remove $i; done
+for i in $(swift-ring-builder /etc/swift/account.builder | grep 6002 | awk '{print $4}' | cut -d ":" -f 1); do
+    if [ "$i" != "$first" ] && [ "$i" != "$second" ] && [ "$i" != "$third" ]; then
+        echo "Removing: $i"
+        sudo swift-ring-builder /etc/swift/account.builder remove $i
+        sudo swift-ring-builder /etc/swift/object.builder remove $i
+        sudo swift-ring-builder /etc/swift/container.builder remove $i
+    fi
+done
 
 sudo swift-ring-builder /etc/swift/object.builder rebalance
 sudo swift-ring-builder /etc/swift/account.builder rebalance
--- scripts/init-cloud.sh
+++ scripts/init-cloud.sh
@@ -20,10 +20,10 @@ openstack floating ip create external
 
 openstack keypair create --public-key /home/stack/.ssh/id_rsa.pub undercloud-stack
 
-openstack flavor create --ram 512 --disk 1 --vcpus 1 --public cirros.1
-openstack flavor create --ram 1024 --disk 1 --vcpus 1 --public small.1
-openstack flavor create --ram 2048 --disk 10 --vcpus 2 --public medium.1
-openstack flavor create --ram 4096 --disk 20 --vcpus 4 --public large.1
+openstack flavor create --ram 512 --disk 1 --vcpus 1 --property hw:mem_page_size=large --public cirros.1
+openstack flavor create --ram 1024 --disk 1 --vcpus 1 --property hw:mem_page_size=large --public small.1
+openstack flavor create --ram 2048 --disk 10 --vcpus 2 --property hw:mem_page_size=large --public medium.1
+openstack flavor create --ram 4096 --disk 20 --vcpus 4 --property hw:mem_page_size=large --public large.1
 
 openstack security group create all-access
 openstack security group rule create --ingress --protocol icmp --src-ip 0.0.0.0/0 all-access
@@ -35,9 +35,11 @@ net_id=$(neutron net-list | grep sriov1 | awk '{print $2}')
 neutron subnet-create $net_id --allocation_pool start=172.21.0.2,end=172.21.1.0 --name sriov1 172.21.0.0/16
 sub_id=$(neutron subnet-list | grep sriov1 | awk '{print $2}')
 neutron port-create --name sriov1 --fixed-ip subnet_id=$sub_id,ip_address=172.21.0.10 --vnic-type direct $net_id
+neutron port-create --name sriov2 --fixed-ip subnet_id=$sub_id,ip_address=172.21.0.11 --vnic-type direct $net_id
 
 neutron net-create --provider:network_type vlan --provider:physical_network physnet_sriov2 --provider:segmentation_id 2100 sriov2
 net_id=$(neutron net-list | grep sriov2 | awk '{print $2}')
 neutron subnet-create $net_id --allocation_pool start=172.22.0.2,end=172.22.1.0 --name sriov2 172.22.0.0/16
 sub_id=$(neutron subnet-list | grep sriov2 | awk '{print $2}')
-neutron port-create --name sriov2 --fixed-ip subnet_id=$sub_id,ip_address=172.22.0.20 --vnic-type direct $net_id
+neutron port-create --name sriov3 --fixed-ip subnet_id=$sub_id,ip_address=172.22.0.20 --vnic-type direct $net_id
+neutron port-create --name sriov4 --fixed-ip subnet_id=$sub_id,ip_address=172.22.0.21 --vnic-type direct $net_id
