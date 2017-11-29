6dda31032d56a4b067edddefd5ccbcdf7603de2b
Andrew Bays <abays@redhat.com>
2017-11-29 15:31:20 -0500

    Working towards successful deploy

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -1,8 +1,10 @@
 openstack overcloud deploy \
 --templates  \
+-r ./templates/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
 -e ./templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/neutron-opendaylight-sriov.yaml \
+-e ./templates/environments/neutron-l2gw-opendaylight.yaml \
 -e ./templates/environments/network-environment.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
 -e ./templates/environments/docker_registry.yaml \
 -e ./templates/ips-from-pool-all.yaml
