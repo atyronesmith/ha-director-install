381d205f6bf13cbd444c1f52dbb81ff08103377c
Andrew Bays <abays@redhat.com>
2017-11-30 09:09:05 -0500

    Deploys but is not working

--- sriov-odl-overcloud-deploy.sh
+++ sriov-odl-overcloud-deploy.sh
@@ -1,8 +1,9 @@
+# this deploys without L2GW, and just does SRIOV+ODL
+
 openstack overcloud deploy \
 --templates  \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
--e ./templates/environments/storage-environment.yaml \
--e ~/templates/environments/neutron-sriov.yaml \
--e ~/templates/environments/neutron-opendaylight-l3.yaml \
+-e ./templates/environments/neutron-sriov.yaml \
+-e ./templates/environments/neutron-opendaylight-l3.yaml \
 -e ./templates/ips-from-pool-all.yaml
