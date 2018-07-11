4595f7c23bc7f7a540e5912162bc03c51aa9efd0
Andrew Bays <abays@redhat.com>
2018-07-11 13:00:27 -0400

    Successfully deploys

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -7,6 +7,6 @@ openstack overcloud deploy \
 -e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
 -e ./templates/environments/network-isolation.yaml \
 -e ./templates/environments/network-environment.yaml \
--e ./templates/environments/docker_registry.yaml \
+-e ~/docker_registry.yaml \
 -e ./templates/environments/neutron-sriov.yaml \
 -e ./templates/ips-from-pool-all.yaml
