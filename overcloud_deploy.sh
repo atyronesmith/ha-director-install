fb99f4747b23fff11a1ddca18b5d7df1ec5c8e4e
Andrew Bays <andrew.bays@gmail.com>
2018-08-01 16:32:22 -0400

    Cleaner overcloud deploy script

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -1,9 +1,4 @@
 openstack overcloud deploy \
---templates  \
--e ./templates/environments/network-isolation.yaml \
--e ./templates/environments/network-environment.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/docker-ha.yaml \
--e ./templates/environments/docker_registry.yaml \
--e ./templates/ips-from-pool-all.yaml
+-r ./templates/roles_data.yaml \
+-p ./templates/plan-environment-derived-params.yaml \
+--answers-file /home/stack/templates/answers.yaml
