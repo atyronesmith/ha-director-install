2ca7a447557bbc3c0df26bf0fdee428cc354f6ec
Andrew Bays <andrew.bays@gmail.com>
2019-05-10 08:39:29 -0400

    Fails at deploy step 2

--- overcloud_deploy.sh
+++ overcloud_deploy.sh
@@ -1,4 +1,4 @@
 openstack overcloud deploy \
 -r ./templates/roles_data.yaml \
 -p ./templates/plan-environment-derived-params.yaml \
---answers-file /home/stack/templates/answers.yaml
+--answers-file /home/stack/ha-director-install/templates/answers.yaml
